39e716668e404228253c9980506f0a4fc10ddd01
Brian Cohen <5685887+bmattlife@users.noreply.github.com>
2023-04-02 14:15:54 -0700

    Initial commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,19 @@
+function TodoList() {
+    return (
+        <div className="TodoList">
+            <div className="row">
+                <div className="column">
+                    <ul>
+                        <li>item1</li>
+                        <li>item2</li>
+                        <li>item3</li>
+                        <li>item4</li>
+                        <li>item5</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default TodoList;
